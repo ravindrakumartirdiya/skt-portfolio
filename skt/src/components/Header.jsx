8e6ba8a769fccd1df597449d04ee093d1ee7e6af
ravindrakumartirdiya <ravindra.19cs054@secs.ac.in>
2022-03-09 17:48:54 +0000

    change3

--- skt/src/components/Header.jsx
+++ skt/src/components/Header.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
-import { Link } from 'react-router';
-import About from './About';
-import Work from './Work';
+import styled from 'styled-components'
+import { Link } from 'react-scroll'
+// import About from './About';
+// import Work from './Work';
 import './Header.css';
+
+const Link1 = styled(Link)`
+&:hover {
+	color: green;
+	transition: 200ms ease-in;
+}
+`;
+
 
 
 const Header = () => {
   return (
-    <div className='Header'>
-        <div className='Menubar'>
-         
-        </div>
+    <div className='Header' >
+      <div className='Logo'><h1>Portfolio</h1></div>
+
+      <div className='Menu'>
+        <div>
+          <Link1 to="./About" smooth={true} duration={500} >
+            About
+
+          </Link1></div>
+        <div >
+          <Link1 to="./Work" smooth={true} duration={500}>
+            Work
+          </Link1></div>
+      </div>
     </div>
   );
 };
